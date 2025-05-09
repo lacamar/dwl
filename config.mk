68bc91c1ec573d8db87ee4ab1ea8b0524c6adcd0
lacamar <lchlnm@pm.me>
2025-05-10 03:28:19 +0800

    enable xwayland support

--- config.mk
+++ config.mk
@@ -4,14 +4,11 @@ VERSION  = `git describe --tags --dirty 2>/dev/null || echo $(_VERSION)`
 PKG_CONFIG = pkg-config
 
 # paths
-PREFIX = /usr/local
+PREFIX = /usr
 MANDIR = $(PREFIX)/share/man
 DATADIR = $(PREFIX)/share
 
-XWAYLAND =
-XLIBS =
-# Uncomment to build XWayland support
-#XWAYLAND = -DXWAYLAND
-#XLIBS = xcb xcb-icccm
+XWAYLAND = -DXWAYLAND
+XLIBS = xcb xcb-icccm
 
 CC = gcc
